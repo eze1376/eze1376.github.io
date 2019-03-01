43745fecec2441c9e8fd709428ae40bafcfafc4d
Erfan Zekri Esfahani <erfanzekri@gmail.com>
2019-03-01 21:48:31 +0330

    Update MyJavaScript1.js

--- Scripts/My Scripts/MyJavaScript1.js
+++ Scripts/My Scripts/MyJavaScript1.js
@@ -1,19 +1,19 @@
 ﻿$(document).ready(function () {
-    $('#menchIcon').attr("src", "Images/Mench1.png");
-    $('#menchIcon').attr("src", "Images/Mench0.png");
+    var img=new Image();
+    img.src="Images/Mench1.png";
 
-    $('#MineIcon').attr("src", "Images/Bomb2.png");
-    $('#MineIcon').attr("src", "Images/Bomb1.png");
+    var img2 = new Image();
+    img2.src = "Images/Bomb2.png";
 
     $("#menchIcon").mouseover(function () {
-        $('#menchIcon').attr("src", "Images/Mench1.png");
+        $('#menchIcon').attr("src", img.src);
     });
     $("#menchIcon").mouseleave(function () {
         $('#menchIcon').attr("src", "Images/Mench0.png");
     });
 
     $("#MineIcon").mouseover(function () {
-        $('#MineIcon').attr("src", "Images/Bomb2.png");
+        $('#MineIcon').attr("src", img2.src);
     });
     $("#MineIcon").mouseleave(function () {
         $('#MineIcon').attr("src", "Images/Bomb1.png");
